012d6c42e0c8cf54df7cbe374f68fee1f53ca031
Mateen <mateenabdullopato@gmail.com>
2024-04-26 04:28:09 +0000

    Refactor AboutPage, HomePage, and ServicesPage components

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import image1 from '../assets/images/Rectangle 11.png'
+import image2 from '../assets/images/Rectangle 8.png'
+import { Contact,Store } from 'lucide-react'
 
 const AboutPage:React.FC = () => {
   return (
-    <div>AboutPage</div>
+    <div className="w-full grid bg-[#fff]  place-items-center">
+    <div className="w-5/6 grid lg:grid-cols-[2fr,3fr] place-items-center">
+      <div className=' w-full h-fit grid justify-between'>
+        
+        <div className='flex flex-col gap-4'>
+        <header className='text-blue-600 font-bold text-xl flex gap-2'>
+          <span><Store/></span>
+          <p>About us</p> </header>
+        <h2 className='text-2xl lg:text-3xl font-bold uppercase'>quickly and easy ways to clean your house</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+          euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+          accumsan et viverra justo commodo.
+        </p>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <h2 className='uppercase text-2xl font-bold text-blue-600 pt-4'>award winning company</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            euismod bibendum laoreet. Proin gravida dolor sit amet lacus
+            accumsan et viverra justo commodo.
+          </p>
+          <div>
+          <button className="bg-blue-600 text-white px-6 py-2 rounded-2xl flex gap-2">
+                <Contact size={24} />
+                <span> Contact Us</span>
+              </button>
+          </div>
+        </div>
+
+        
+      </div>
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
+  <div className="col-span-1 row-span-2  grid place-items-center">
+    <img src={image1} alt="image1" className='w-full h-1/2 rounded-md'/>
+  </div>
+  <div className="grid place-items-center -mb-12">
+    <img src={image2} alt="image2"  className='w-full h-3/5  rounded-md'/>
+  </div>
+  <div className=" grid place-items-center -mt-12">
+    <img src={image1} alt="image1" className='w-full h-3/5  rounded-md'/>
+  </div>
+
+</div>
+
+    </div>
+  </div>
   )
 }
 
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from './Hero'
+import AboutPage from '../AboutPage'
+import ServicesPage from '../ServicesPage'
 
 const HomePage:React.FC = () => {
   return (
     <div className=''>
       <Hero />
+      <div className='py-5'>
+        <AboutPage />
+      </div>
+      <div>
+        <ServicesPage />
+      </div>
     </div>
   )
 }
