dd88e94b29414826819739ead7bb0ce5e6a9cafb
Masood Acheampong <masoodacheampong@gmail.com>
2024-04-27 03:29:24 +0100

    Completed implementing the cctv page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
 // App.js
-import React,{Suspense} from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Loading from './components/Loading';
-const Home = React.lazy(() => import('./pages/Home/HomePage'));
-const AboutPage = React.lazy(() => import('./pages/AboutPage'));
-const ContactPage = React.lazy(() => import('./pages/ContactPage'));
-const ServicesPage = React.lazy(() => import('./pages/ServicesPage'));
-const Navbar = React.lazy(() => import('./components/Navbar'));
-const Footer = React.lazy(() => import('./components/Footer'));
-
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Loading from "./components/Loading";
+import CCTVPage from "./pages/CCTVPage";
+const Home = React.lazy(() => import("./pages/Home/HomePage"));
+const AboutPage = React.lazy(() => import("./pages/AboutPage"));
+const ContactPage = React.lazy(() => import("./pages/ContactPage"));
+const ServicesPage = React.lazy(() => import("./pages/ServicesPage"));
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
 
 function App() {
   return (
-    <div className='w-full grid place-items-center'>
-    <BrowserRouter>
-      <Suspense fallback={<Loading/>}>
-        <Navbar />
-        <Routes>
-          <Route index={false} path="/" element={<LandingPage />} >
-            <Route index path="/" element={<Home />} />
-            <Route path="#about" element={<AboutPage />} />
-            <Route path="#contact" element={<ContactPage />} />
-            <Route path="#services" element={<ServicesPage />} />
-        </Route>
-        </Routes>
-        <Footer />
-      </Suspense>
-    </BrowserRouter>
+    <div className="w-full grid place-items-center">
+      <BrowserRouter>
+        <Suspense fallback={<Loading />}>
+          <Navbar />
+          <Routes>
+            <Route index={false} path="/" element={<LandingPage />}>
+              <Route index path="/" element={<Home />} />
+              <Route path="#about" element={<AboutPage />} />
+              <Route path="#contact" element={<ContactPage />} />
+              <Route path="#services" element={<ServicesPage />} />
+            </Route>
+            <Route path="/cctv" element={<CCTVPage />} />
+          </Routes>
+          <Footer />
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 }
